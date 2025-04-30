84226383c3854df21e3af91cef94433e0caa1766
Sava17524Love <144526618+Sava17524Love@users.noreply.github.com>
2025-04-30 15:26:20 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ import numpy as np
 from flask import Flask, render_template
 import os
 
+
 def index():
     data = pd.read_csv('date.csv')
-    photo = ''
     name = data[data['category'] == 'name']['text'].values[0]
     position = data[data['category'] == 'position']['text'].values[0]
+    summary = data[data["category"] == "summary"]["text"].values[0]
     contacts = data[data['category'] == 'contacts'][['text', 'link']].replace({np.nan: None}).values
     skills = data[data['category'] == 'skills']['text'].values
     projects = data[data['category'] == 'projects'][['text', 'link']].replace({np.nan: None}).values
@@ -15,9 +16,9 @@ def index():
     achievements = data[data['category'] == 'achievements']['text'].values
     facts = data[data['category'] == 'facts']['text'].values
 
-    return render_template('index.html', name=name,
-                           #photo=photo,
+    return render_template('index.html', name=name, summary=summary,
                            position=position, contacts=contacts, skills=skills, projects=projects, education=education, achievements=achievements, facts=facts)
+
 
 folder = os.getcwd()
 app = Flask(__name__, template_folder=folder, static_folder=folder)
